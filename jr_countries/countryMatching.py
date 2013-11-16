a535773f63e926085a4ae2e8323e3d45539df46c
jonroberts <jproberts00@gmail.com>
2013-11-16 10:31:58 -0500

    Added an iso country list and started some d3 outlines

---
+++ jr_countries/countryMatching.py
@@ -0,0 +1,17 @@
+#!/Library/Frameworks/EPD64.framework/Versions/Current/bin/python
+
+import sys
+import matplotlib
+
+import pylab
+import numpy as np
+from numpy import *
+import scipy as sp
+from scipy import cluster
+from scipy import optimize
+import matplotlib.pyplot as plt
+from matplotlib import cm
+
+from pylab import *
+import math
+import os
